5f34c92412d50f14ad5682b16ca086a15601e64c
Valery Petrov <valeryvpetrov.dev@gmail.com>
2022-11-20 16:48:10 +0300

    Add game loop

--- 15puzzle.sh
+++ 15puzzle.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
 declare -i step=1
 
-echo "Ход № ${step}"
+while :
+do
+	echo "Ход № ${step}"
+	read -p "Ваш ход (q - выход):" answer
+	if [[ "$answer" =~ ^[0-9]+$ ]]; then
+		if ((1<=answer && answer<=15)); then
+			echo "Ход - ${answer}"
+		else
+			echo "Неверный ход! Можно ввести число от 1 до 15."
+		fi
+	else
+		if [[ "$answer" == "q" ]]; then
+			echo "Пока!"
+			exit 0
+		else
+			echo "Неверный ход! Можно ввести число от 1 до 15."
+		fi
+	fi
+	step+=1
+done
