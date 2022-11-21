4aa6364b348422ff36e123a5a363e18977bd15c9
Valery Petrov <valeryvpetrov.dev@gmail.com>
2022-11-21 19:50:46 +0300

    Add print matrix function

--- 15puzzle.sh
+++ 15puzzle.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
+function init_matrix () {
+	for ((i=0;i<$num_rows;i++)) do
+		for ((j=0;j<$num_columns;j++)) do
+			value=$(( i*num_columns + j ))
+			matrix+=($value)
+		done
+	done
+}
+
+function print_matrix () {
+	echo "+-------------------+"
+	for ((i=0;i<$num_rows;i++)) do
+		for ((j=0;j<$num_columns;j++)) do
+			num=${matrix[$(( i*num_columns + j ))]}
+			if [ $num -eq 0 ]; then
+				num_repr="  "
+			elif (( 0<=num && num<=9 )); then
+				num_repr="${num} "
+			else
+				num_repr="${num}"
+			fi
+			echo -n "| ${num_repr} "
+			if (( j==$(( num_rows - 1 )) )); then
+				echo "|"
+			fi
+		done
+		if (( 0<=i && i<$(( num_rows - 1 )) )); then
+			echo "|-------------------|"
+		fi
+	done
+	echo "+-------------------+"
+}
+
 declare -i step=1
+declare -a matrix
+num_rows=4
+num_columns=4
+nums_len=$((num_rows*num_columns))
+init_matrix
 
 while :
 do
 	echo "Ход № ${step}"
+	print_matrix
 	read -p "Ваш ход (q - выход):" answer
 	if [[ "$answer" =~ ^[0-9]+$ ]]; then
 		if ((1<=answer && answer<=15)); then
