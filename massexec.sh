79695aba66ceb390a321ed80c4b3c1963fb7673b
Valery Petrov <valeryvpetrov.dev@gmail.com>
2022-12-11 13:49:43 +0300

    Parse input args

--- massexec.sh
+++ massexec.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo "massexec.sh"
+function usage() {
+	echo "massexec.sh [--path dirpath] [--mask mask] [--number number] command"
+	exit 1
+}
+
+# Parse input args
+while :; do
+    case $1 in
+        --path) path=$2 ;;
+        --mask) mask=$2 ;;
+        --number) number=$2 ;;
+        *) 
+			if (( $# == 1 )); then
+				command=$1
+				break
+			else
+				usage
+			fi
+		;;
+    esac
+    shift; shift
+done
+
+echo "path=$path"
+echo "mask=$mask"
+echo "number=$number"
+echo "command=$command"
